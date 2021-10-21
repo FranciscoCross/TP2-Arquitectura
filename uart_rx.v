fdcfb8477bedc78750ea00c4231b7eb418e337d0
FranciscoCross <francisco.delacruz@mi.unc.edu.ar>
2021-10-21 18:13:39 -0300

    Comentarios para entender RX

--- uart_rx.v
+++ uart_rx.v
@@ -1,3 +1,20 @@
+/*RECEPTOR 
+    NB_BIT constante que indica el numero de bits de datos
+    SB_TICK constante que indica el numero de ticks que se necesitan para los bits de stop (16,24,32) para (1, 1.5, 2) bits de stop
+
+    Tres estados mayores start data stop que representan el proceso de start bit, bits de datos, bit de stop
+    La "s_tick" signal es el tick habilitador del baudrate generator y hay 16 ticks en un interbalo de bits
+
+    Luego tenemos dos contadores "s" (mantienen un registro de cuenta sobre los ticks) y "n" (realiza un seguimiento de los bits de datos recibidos)
+    los bits de datos recuperados se shiftean y de ensamblan en "b" 
+    
+    rx_done_tick indica cuando ya se tiene el dato completo (todos los bits recibidos)
+    
+    AGREGAR BIT DE PARIDAD (UN NUEVO ESTADO)
+    
+    */
+
+
 module uart_rx
     # ( 
     parameter NB_BIT = 8, // # cantidad de bits por dato 
@@ -66,48 +83,51 @@ module uart_rx
                 end
             start:
                 begin
-                    if (s_tick) 
+                    if (s_tick)                                 
                         begin
-                            if (s_reg == 7)
+                            if (s_reg == ((SB_TICK/2) - 1))           //chequiamos que el tick sea la mitad del muestreo
                                 begin
-                                    state_next = data;
-                                    s_next = 0;
-                                    n_next = 0;
+                                    state_next = data;                //si es asi pasamos al estados para recibir los datos
+                                s_next = 0;                           //limpiamos el registro que cuenta los ticks
+                                    n_next = 0;                       //limpiamos el registro que lleva la cuenta de los bits recibidos
                                 end    
                         end
                     else
-                        s_next = s_reg + 1;
+                        s_next = s_reg + 1;                           //si no seguimos contando los ticks
                 end
             data:
                 begin
                     if (s_tick)
                         begin
-                            if (s_reg == 15)
+                            if (s_reg == (SB_TICK - 1))
                                 begin
-                                    s_next = 0;
-                                    b_next = {rx, b_reg[NB_BIT - 1:1]};
-                                    if(n_reg == (DBIT-1))
-                                        state_next = stop;
+                                    s_next = 0;                         //tenemos el bit por ende reiniciamos el contador de ticks
+                                    b_next = {rx, b_reg[NB_BIT - 1:1]}; //ponemos el bit en el registro b 
+                                    if(n_reg == (DBIT-1))               //chequiamos si es el ultimo bit de lo necesario (8)
+                                        state_next = stop;              //si es asi pasamos al estado stop
                                     else
-                                        n_next = n_reg + 1;    
+                                        n_next = n_reg + 1;             //si aumentamos el contador que lleva la cuenta de los bits recibidos
                                 end    
                         end
                     else
-                        s_next = s_reg + 1;
+                        s_next = s_reg + 1;                              //si no seguimos contando los ticks
                 end
             stop:
                 begin
                     if (s_tick)
                         begin
-                            if (s_reg == (SB_TICK - 1))
-                                begin
-                                    state_next = idle;
-                                    rx_done_tick = 1'b1;
+                            if (s_reg == (SB_TICK - 1))                 //corroboramos que los ticks son los necesarios para el bit de stop
+                                begin   
+                                    state_next = idle;                  //si es asi volvemos a idle
+                                    rx_done_tick = 1'b1;                //y ponemos que tenemos el dato completo
                                 end
                             else
-                                s_next = s_reg + 1;
+                                s_next = s_reg + 1;                     //si no seguimos contando los ticks
                         end
                 end
+            
+            
+            //Default que agregamos nosotros porque no tenia
             default:
                 begin
                     state_next  = IDLE;
